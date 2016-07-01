a1ab7bd06510b5079df0ab09f25b55184aecc1d4
Aliaksei Zhukau <azhukov87@gmail.com>
2016-07-01 18:11:37 +0300

    Adding Angular

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+﻿// MODULE
+var wheatherApp = angular.module("wheatherApp", ["ngRoute", "ngResource"]);
+
+//ROUTES
+wheatherApp.config(function ($routeProvider) {
+
+    $routeProvider.
+
+    when("/", {
+        templateUrl: "forecast.html",
+        controller: "homeController"
+    }).
+
+    when("/forecast", {
+        templateUrl: "pages/forecast.htm",
+        controller: "forecastController"
+    });
+
+});
+
+//CONTROLLERS
+wheatherApp.controller("homeController", ["$scope", function ($scope) {
+
+}]);
+
+wheatherApp.controller("forecastController", ["$scope", function ($scope) {
+
+}]);
